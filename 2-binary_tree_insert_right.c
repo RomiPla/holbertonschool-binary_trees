c5aac78b6da6ab2bd2033cceab54b2a91c6ef1f5
Nacho <146840051+rivero04@users.noreply.github.com>
2024-01-06 13:30:19 -0300

    tasks binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,19 +16,21 @@ include "binary_trees.h"
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+	binary_tree_t *old_right;
 
 	if (parent == NULL)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
-
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->right != NULL)
+	old_right = parent->right;
+	
+	if (old_right != NULL)
 	{
-		new_node->right = parent = right;
-		parent->right->parent = new_node;
+		new_node->right = old_right;
+		old_right->parent = new_node;
 	}
 	parent->right = new_node;
 
