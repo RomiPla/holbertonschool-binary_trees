e204eaab25e061410729387bf79a48cb96019c83
Nacho <146840051+rivero04@users.noreply.github.com>
2024-01-06 14:38:38 -0300

    Fix many error

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,12 +5,12 @@
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (func && tree)
 	{
 		func(tree->n);
-		binary_tree_inorder(tree->left, func);
-		binary_tree_inorder(tree->right, func);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
 	}
 }
